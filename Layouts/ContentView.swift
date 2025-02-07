f2814f5ee75c324af53a19a0eab80e94440097ad
dljardim <dljardim@outlook.com>
2025-02-07 16:58:30 -0600

    SwiftUI Layout Fundamentals - Examples
    .border
    .frame
    .infinity
    .minimumScaleFactor
    .multilineTextAlignment
    .alignment
    spacing
    view default values

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,12 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var messageString1:String = "When the Genius Bar Needs Help, They call You!!"
+    @State private var messageString2:String = "Great"
+    @State private var messageString3:String = "Amazing Grace"
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            Text(messageString1)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .padding()
+                .frame(width: 200,height: 200)
+                .border(.orange, width: 2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+
+            
+            Text(messageString2)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .border(.orange, width: 2)
+                
+
+            
+            Text(messageString3)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .padding()
+                .frame(width: 200,height: 200)
+                .border(.orange, width: 2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+            
+            HStack{
+                Button("Genius"){
+                    // messageString1 = "When the Genius Bar Needs Help, They call You!!"
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.accentColor)
+                
+                Button("Great"){
+                    // messageString2 = "Great"
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.accentColor)
+                
+                
+                Button("Amazing"){
+                    // messageString3 = "Amazing Grace"
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.accentColor)
+            }
+            .border(.orange, width: 2)
+
+            
         }
         .padding()
     }
